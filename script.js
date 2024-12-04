43749f1ec1668e92c8939299a1783e2328644923
Jayant <goyal151002@gmail.com>
2024-12-04 13:40:20 +0530

    sidemenu created

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle the side menu on hamburger click
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.getElementById("sidemenu").classList.toggle("active");
+});
